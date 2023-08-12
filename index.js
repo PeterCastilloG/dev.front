8776994447897a35bc29517493c3163cc5a86eaf
PeterCastilloG <peter.castillo@globals.one>
2023-08-12 16:13:56 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const email = document.querySelector(".email");
+const password = document.querySelector(".pass");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", async () => {
+  const emailValue = email.value;
+  const passValue = password.value;
+  try {
+    const response = await post(emailValue, passValue);
+    if (response.ok) {
+      const result = await response.json();
+      console.log(result);
+    } else {
+      console.error("Error en la solicitud:", response.status, response.statusText);
+    }
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+  }
+});
+
+const post = async (email, pass) => {
+  return await fetch("http://localhost:3000/form", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      pass: pass,
+    }),
+  });
+};
